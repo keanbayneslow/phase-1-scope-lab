341c7567514af3171dcc8a62556178bab0de8673
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-08-16 14:38:50 +0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase();
+};
+
+var setBestCustomer = () => {
+    bestCustomer = 'not bob';
+};
+
+var overwriteBestCustomer = newFavorite => {
+    bestCustomer = newFavorite;
+};
+
+const leastFavoriteCustomer = 'mike';
+
+var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'someone else';
+};
